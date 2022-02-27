19272934dfbb15a2665b8c55058f94b0e55d2879
Petr Douda <doudap@centrum.cz>
2022-02-27 18:34:12 +0100

    Implemented AML Element lookup by name

--- Examples/main.py
+++ Examples/main.py
@@ -1,38 +1,17 @@
-import lxml.objectify as ob
-from pyautomationml import PyAutomationML, Element
-
-
-def print_tree(root, depth):
-    print(" "*depth + (str(root.get("Name")) if root.get("Name") is not None else root.tag))
-    for child in root.iterchildren():
-        print_tree(child, depth+4)
+from pyautomationml import PyAutomationML
 
 
 if __name__ == "__main__":
     #   load an AML file located at ./Testbed.aml
     doc = PyAutomationML("./Testbed.aml")
 
-    print(type(doc.root.Testbed.ControlSystem.MES.__element__))
-    """
-    t0 = doc.root.find("./InstanceHierarchy[@Name='Testbed']")
-    t1 = doc.root.get_child_by_name("Testbed")
+    #   Set up AML element lookup by AML name (for example doc.root.Testbed instead of doc.root.InstanceHierarchy)
+    doc.root.__lookup_by_name__ = True
 
-    t2 = doc.root.Testbed"""
-
-    doc.save("Instantiated.aml")
-    #print_tree(doc.root, 0)
-    """
-    opcua_vars = doc.root.findall(".//Attribute[@Name='OPCUA-Variables']")
-    for vars in opcua_vars:
-        for var in vars.iterchildren():
-            var.set("RefAttributeType", "PyAMLLib/OPCUA-Variable")
-    doc.save("Testbed.aml")"""
-
-    """
     #   To browse the AML files methods from lxml objectify are available (see https://lxml.de/objectify.html)
     #   Moreover additional methods are available
-    #       - element("name") which returns a child AML InternalElement of a given "name" if it exists
-    #       - attribute("name") which returns a child AML Attribute of a given "name" if it exists
+    #       - get_element_by_id("ID") which returns an AML InternalElement of a given "ID" if it exists
+    #       - get_linked_interface() which can be called on an ExternalInterface and returns the ExternalInterface linked to it.
     #       - text() which returns the Value of an element if it is an AML Attribute
     #   The root element of the xml tree is in this case located at doc.root
     MES = doc.root.Testbed.ControlSystem.MES
@@ -45,7 +24,7 @@ if __name__ == "__main__":
     #   instantiate the expressions in the AML file Testbed.aml
     #   the expressions are evaluated in a separate context containing only python builtins,
     #   the ancestors variable and locals and globals defined in the file preamble.py
-    doc.eval()
+    doc.eval(verbose=True)
 
     #   print resulting instantiated expressions from Testbed.aml after evaluation
     print()
@@ -57,7 +36,7 @@ if __name__ == "__main__":
     #   note that these functions cannot be called before evaluation
     #   because context variables used in the functions are not defined before evaluation
     print(MES.CurrentTime.fun())
-    print(MES.InputData.fun(data="This is my data"))"""
+    print(MES.InputData.fun(data="This is my data"))
 
     #   save the instantiated AML file under a new name
-    #doc.save("Instantiated.aml")
+    doc.save("Instantiated.aml")
--- pyautomationml/pyautomationml.py
+++ pyautomationml/pyautomationml.py
@@ -10,59 +10,69 @@ def Element(tag, attrib=None, text=None, context_global=None, context_local=None
 
 
 class AmlElement:
-    def __init__(self, element, context_global, context_local):
+    def __init__(self, element, context_global, context_local, lookup_by_name=False):
+        self.__lookup_by_name__ = lookup_by_name
         self.__element__ = element
         self.__context_local__ = context_local
         self.__context_global__ = context_global
 
     def __getattr__(self, key):
         if key == "fun":
-            self.__context_local__.update({"ancestors": self.ancestors()})
-            result = eval(self.text(), self.__context_global__, self.__context_local__)
+            self.__context_local__.update({"ancestors": self.iterancestors()})
+            result = eval(self.text().strip(), self.__context_global__, self.__context_local__)
             self.__context_local__.pop("ancestors")
         else:
-            result = self.find(f"./*[@Name='{key}']")
-            if result is None:
+            if self.__lookup_by_name__:
+                result = self.find(f"./*[@Name='{key}']")
+                if result is None:
+                    result = self.__element__.__getattribute__(key)
+                    if isinstance(result, ob.ObjectifiedElement):
+                        raise AttributeError(f"No such child: {key}")
+            else:
                 result = self.__element__.__getattribute__(key)
             if isinstance(result, ob.ObjectifiedElement):
-                result = AmlElement(result, self.__context_global__, self.__context_local__)
+                result = AmlElement(result, self.__context_global__, self.__context_local__, self.__lookup_by_name__)
         return result
 
     def __setattr__(self, key, value):
-        if key in ("__element__", "__context_local__", "__context_global__"):
+        if key in ("__element__", "__context_local__", "__context_global__", "__lookup_by_name__"):
             self.__dict__[key] = value
         else:
             if isinstance(value, AmlElement):
                 value = value.__element__
-            element = self.find(f"./*[@Name='{key}']")
-            if element is not None:
-                tag = element.tag
-                element.clear()
-                self.__element__.__setattr__(tag, value)
+            if self.__lookup_by_name__:
+                element = self.find(f"./*[@Name='{key}']")
+                if element is not None:
+                    tag = element.tag
+                    element.clear()
+                    self.__element__.__setattr__(tag, value)
             else:
                 self.__element__.__setattr__(key, value)
 
     def __repr__(self):
         return f"<AmlElement object {self.__element__.tag} Name={self.__element__.get('Name')}>"
 
-    def ancestors(self):
-        return [AmlElement(i, self.__context_global__, self.__context_local__) for i in [self.__element__] + list(self.__element__.iterancestors())]
+    def iterancestors(self):
+        return [AmlElement(i, self.__context_global__, self.__context_local__, self.__lookup_by_name__) for i in [self.__element__] + list(self.__element__.iterancestors())]
 
     def iterchildren(self):
         result = self.__element__.iterchildren()
-        return [AmlElement(i, self.__context_global__, self.__context_local__) for i in result] if result is not None else None
+        return [AmlElement(i, self.__context_global__, self.__context_local__, self.__lookup_by_name__) for i in result] if result is not None else None
 
     def find(self, path):
         result = self.__element__.find(path)
-        return AmlElement(result, self.__context_global__, self.__context_local__) if result is not None else None
+        return AmlElement(result, self.__context_global__, self.__context_local__, self.__lookup_by_name__) if result is not None else None
 
     def findall(self, path):
         result = self.__element__.findall(path)
-        return [AmlElement(i, self.__context_global__, self.__context_local__) for i in result] if result is not None else None
+        return [AmlElement(i, self.__context_global__, self.__context_local__, self.__lookup_by_name__) for i in result] if result is not None else None
 
-    def linked_element(self):
+    def get_element_by_id(self, id):
+        return self.find(f".//*[@ID='{id}']")
+
+    def get_linked_interface(self):
         if self.tag == "ExternalInterface":
-            ancestors = self.ancestors()
+            ancestors = self.iterancestors()
             id = ancestors[1].find(f"./InternalLink[@RefPartnerSideA='{self.get('ID')}']").get("RefPartnerSideB")
             return ancestors[-1].find(f".//ExternalInterface[@ID='{id}']")
         else:
@@ -83,35 +93,45 @@ class PyAutomationML:
         evaluated = 0
         not_evaluated = 0
         errors = 0
+        lookup_cfg = self.root.__lookup_by_name__
+        self.root.__lookup_by_name__ = True
+        preamble = self.root.find("./InstanceHierarchy//InternalElement[@Name='PythonPreamble']").Content
 
-        preamble_file = self.root.find(
-            "./InstanceHierarchy//InternalElement[@Name='PythonPreamble']/ExternalInterface/Attribute[@Name='refURI']").text()
-        if preamble_file is not None:
-            with open(preamble_file) as preamble:
-                exec(preamble.read(), self.context_global, self.context_local)
+        if preamble is not None:
+            self.context_local.update({"ancestors": preamble.iterancestors()})
+            try:
+                exec(preamble.text().strip(), self.context_global, self.context_local)
+            except Exception as e:
+                if verbose:
+                    print("Error in line {0} in file {1}\n          {2} \n{3}\n".format(preamble.sourceline + 1, self.source_file, preamble.text(), e), file=sys.stderr)
+            else:
+                if verbose:
+                    print("Context from Python preamble successfully created", file=sys.stderr)
+            self.context_local.pop("ancestors")
+        else:
+            if verbose:
+                print("No Python preamble found continuing with builtin context", file=sys.stderr)
 
         expression_elements = self.root.findall("./InstanceHierarchy//Attribute[@RefAttributeType='PyAMLLib/PythonExpression']")
-
         for element in expression_elements:
+            self.context_local.update({"ancestors": element.iterancestors()})
             try:
-                self.context_local.update({"ancestors": element.ancestors()})
-                result = eval(element.text(), self.context_global, self.context_local)
-                self.context_local.pop("ancestors")
+                result = eval(element.text().strip(), self.context_global, self.context_local)
             except Exception as e:
                 errors += 1
-                print("Error in line {0} in file {1}\n          {2} \n{3}\n".format(element.sourceline+1,
-                                                                                    self.source_file,
-                                                                                    element.text(),
-                                                                                    e), file=sys.stderr)
+                if verbose:
+                    print("Error in line {0} in file {1}\n          {2} \n{3}\n".format(element.sourceline+1, self.source_file, element.text(), e), file=sys.stderr)
             else:
                 if not callable(result):
                     evaluated += 1
-                    element.Value._setText(str(result))
+                    element.__element__.Value._setText(str(result))
                 else:
                     not_evaluated += 1
+            self.context_local.pop("ancestors")
 
         self.root.__context_local__ = self.context_local
         self.root.__context_global__ = self.context_global
+        self.root.__lookup_by_name__ = lookup_cfg
 
         if verbose:
             print("{0} total PythonExpression{1} found".format(len(expression_elements), "s" if len(expression_elements) > 1 else ""), file=sys.stderr)
@@ -119,8 +139,8 @@ class PyAutomationML:
             print("{0} PythonExpression{1} could not be instantiated as literal{2} because {3} functions.".format(
                 not_evaluated, "s" if not_evaluated > 1 else "", "s" if not_evaluated > 1 else "", "they are" if not_evaluated > 1 else "it is a"), file=sys.stderr)
             if errors:
-                print("{0} PythonExpression{1} could not be instantiated as literal{2} because of errors.".format(
-                    errors, "s" if errors > 1 else "", "s" if errors > 1 else ""), file=sys.stderr)
+                print("{0} PythonExpression{1} could not be instantiated because of errors.".format(
+                    errors, "s" if errors > 1 else ""), file=sys.stderr)
 
     def save(self, filename=None):
         if not filename:
